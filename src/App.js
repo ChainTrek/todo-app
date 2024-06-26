704e84915733890e8148e1c8acf5dd994d393c9c
Elijah Cravens <elijahcravens46@gmail.com>
2024-06-26 14:51:12 -0600

    Update npm dependencies for fontawesome. Created Footer component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
 	return (
 		<>
 			<Header title={'Todo App'} />
+			<Footer text={'© 2024 Elijah W. Cravens'} />
 		</>
 	)
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = ({text}) => {
+  return (
+    <footer>
+      <div className="container">
+        <p>{text}</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
